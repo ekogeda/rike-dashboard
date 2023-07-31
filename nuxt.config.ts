c07a47a4d041faed161d1427e9ade06e222c3f47
Demola <64336052+ekogeda@users.noreply.github.com>
2023-07-31 16:08:23 +0100

    mdi updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	alias: { assets: '/<rootDir>/assets' },
+	// alias: { assets: '/<rootDir>/assets' },
 	css: ['~/assets/main.scss', 'vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
 	build: {
 		transpile: ['vuetify'],
